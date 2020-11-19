d00ea9cf34ae3a22c4978eb5df7b28bcc66b9fb6
Killian Storm <killianstorm@gmail.com>
2020-11-19 15:00:03 +0100

    update to ZooplaApi

--- utils/ZooplaApi.js
+++ utils/ZooplaApi.js
@@ -2,20 +2,21 @@ import axios from 'axios';
 
 const getUrlForSearchOnPlaceName = value => {
   const data = {
-    country: 'uk',
-    pretty: '1',
-    encoding: 'json',
-    listing_type: 'buy',
-    action: 'search_listings',
-    page: 1,
-    place_name: value
+    // country: 'uk',
+    // pretty: '1',
+    api_key: '8u5b2ey3r3p26j8fvchsfeyr',
+    // encoding: 'json',
+    // listing_type: 'buy',
+    // action: 'search_listings',
+    // page: 1,
+    area: value
   };
 
   const querystring = Object.keys(data)
     .map(k => `${k}=${encodeURIComponent(data[k])}`)
     .join('&');
 
-  return `http://api.nestoria.co.uk/api?${querystring}`;
+  return `https://api.zoopla.co.uk/api/v1/property_listings.js?${querystring}`;
 };
 
 export const fetch = async placeName => {
@@ -23,6 +24,6 @@ export const fetch = async placeName => {
 
   return axios.get(url).then(json => {
     console.log(json);
-    return json.data.response;
+    return json;
   });
 };
